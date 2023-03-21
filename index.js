3206917f12c017cf4e65f94d2c13c9d09d082d64
hopemansfield <hopemansfield@gmail.com>
2023-03-21 17:14:40 -0400

    beginning set of questions added, 'view all departments' returns a list of departments from the company_db database;

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const { query } = require("express");
 const express = require("express");
+const inquirer = require("inquirer");
 const mysql = require("mysql2");
-require('console.table');
+require("console.table");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,18 +24,78 @@ const db = mysql.createConnection(
 );
 
 // Query database to show employees' information
-db.query("SELECT employee.first_name, employee.last_name, employee.role_id FROM employee JOIN roles ON employee.role_id = roles.title", function (err, res) {
-  console.log(res);
-  console.table(res);
+// db.query("SELECT employee.first_name, employee.last_name, employee.role_id FROM employee INNER JOIN roles ON employee.role_id = roles.id", function (err, res) {
+//   console.log(res);
+//   console.table(res);
+// });
+
+db.connect((err) => {
+  if (err) throw err;
+  beginPrompts();
 });
 
-// db.query("SELECT employee.first_name AS id, RIGHT JOIN roles ON employee.role_id = roles.id", function (err, res) {
-//     console.log(res);
-//   });
+async function beginPrompts() {
+  const begin = [
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "begin",
+      choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update An Employee Role",
+        "Exit",
+      ],
+    },
+  ];
+  const userBegin = await inquirer.prompt(begin);
+  switch (userBegin.begin) {
+    case "View All Departments":
+      viewDepartments();
+      break;
+    case "View All Roles":
+      viewRoles();
+      break;
+    case "View All Employees":
+      viewEmployees();
+      break;
+    case "Add Department":
+      addDepartment();
+      break;
+    case "Add Role":
+      addRole();
+      break;
+    case "Add Employee":
+      addEmployee();
+      break;
+    case "Update An Employee Role":
+      updateRole();
+      break;
+    case "Exit":
+      db.end(); //need to figure out what this is supposed to do
+      break;
+  }
+};
+
+function viewDepartments() {
+  const departments = `SELECT department.name FROM department`;
+  db.query(departments, (err, res) => {
+    if (err) throw err;
+    console.log('ALL DEPARTMENTS:');
+    console.table(res);
+    beginPrompts();
+    
+  })
+}
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 app.listen(PORT, () => {
@@ -45,6 +107,5 @@ app.listen(PORT, () => {
 // LEFT JOIN movies
 // ON reviews.movie_id = movies.id
 // ORDER BY movies.movie_name;
-
 
 // FROM employee JOIN roles ON employee.role_id = roles.id
